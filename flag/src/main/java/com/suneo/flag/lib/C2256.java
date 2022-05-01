35078e5423b0e3761e4c6ce9551eb1582a360114
alpacaYiChun <chenyichun1987@gmail.com>
2022-05-01 14:40:35 -0700

    fffff

--- flag/src/main/java/com/suneo/flag/lib/C2256.java
+++ flag/src/main/java/com/suneo/flag/lib/C2256.java
@@ -1,5 +1,25 @@
 package com.suneo.flag.lib;
 
 public class C2256 {
-
+    public int minimumAverageDifference(int[] nums) {
+    	long sum = 0;
+    	for(int i=0;i<nums.length;i++) {
+    		sum+=nums[i];
+    	}
+    	long front = 0;
+    	int ret = -1;
+    	long min = Long.MAX_VALUE;
+    	for(int i=0;i<nums.length;i++) {
+    		front+=nums[i];
+    		long rear = sum-front;
+    		long avgFront = front/(i+1);
+    		long avgRear = rear == 0? 0: rear/(nums.length - i -1);
+    		long abs = Math.abs(avgFront - avgRear);
+    		if(abs<min) {
+    			min = abs;
+    			ret = i;
+    		}
+    	}
+    	return ret;
+    }
 }
